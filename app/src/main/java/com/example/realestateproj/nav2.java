8cc3fd73af57e2782b8f59c95a60a753530cf96e
NilamLokhande <nilamlokhande2303@gmail.com>
2021-03-28 15:55:38 +0530

    line added

---
+++ app/src/main/java/com/example/realestateproj/nav2.java
@@ -0,0 +1,14 @@
+package com.example.realestateproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class nav2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nav2);
+    }
+}
