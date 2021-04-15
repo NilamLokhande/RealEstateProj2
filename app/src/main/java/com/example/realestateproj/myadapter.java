0cb98860c9d19f004b5d514ca91ed9bb112b90c8
barkenikhil02 <barkenikhil02@gmail.com>
2021-04-15 11:39:53 +0530

    searching

--- app/src/main/java/com/example/realestateproj/myadapter.java
+++ app/src/main/java/com/example/realestateproj/myadapter.java
@@ -1,5 +1,4 @@
 package com.example.realestateproj;
-
 import android.content.Context;
 import android.content.Intent;
 import android.view.LayoutInflater;
@@ -15,16 +14,27 @@ import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 
-public class myadapter extends FirebaseRecyclerAdapter<model,myadapter.myviewholder> {
-    private Context context;
+import java.util.ArrayList;
 
-    public myadapter(@NonNull FirebaseRecyclerOptions<model> options) {
-        super(options);
+
+public class myadapter extends RecyclerView.Adapter<myadapter.MyViewHolder>{
+    Context context;
+
+
+    ArrayList<model> list;
+    public myadapter(ArrayList<model> list){
+        this.list = list;
+    }
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.singlerowdesign, parent, false);
+        return new MyViewHolder(view);
     }
 
     @Override
-    protected void onBindViewHolder(@NonNull myviewholder holder, int position, @NonNull model model) {
-
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        model model = list.get(position);
         final String name = model.getName();
         final String description = model.getPropDesc();
         final String ownName = model.getmPropOwnerName();
@@ -36,12 +46,6 @@ public class myadapter extends FirebaseRecyclerAdapter<model,myadapter.myviewhol
         holder.nametext.setText(model.getName());
         holder.descriptiontext.setText(model.getPropDesc());
         Glide.with(holder.img1.getContext()).load(url).into(holder.img1);
-//        try {
-//            URL url1 = new URL(model.getPurl());
-//            Bitmap image = BitmapFactory.decodeStream(url1.openConnection().getInputStream());
-//        } catch(IOException e) {
-//            System.out.println(e);
-//        }
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -62,27 +66,113 @@ public class myadapter extends FirebaseRecyclerAdapter<model,myadapter.myviewhol
         });
     }
 
-
-    @NonNull
     @Override
-    public myviewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.singlerowdesign,parent,false);
-        return new myadapter.myviewholder(view);
+    public int getItemCount() {
+        return list.size();
     }
 
-    public class myviewholder extends RecyclerView.ViewHolder {
+    class MyViewHolder extends RecyclerView.ViewHolder{
+        ImageView img1; TextView nametext; TextView descriptiontext;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            context = itemView.getContext();
+            img1=itemView.findViewById(R.id.img1);
+            nametext=itemView.findViewById(R.id.nametext);
+            descriptiontext=itemView.findViewById(R.id.descriptiontext);
+        }
+    }
 
-      ImageView img1; TextView nametext; TextView descriptiontext;
-
-      public myviewholder(@NonNull View itemView) {
-          super(itemView);
-          context = itemView.getContext();
-          img1=itemView.findViewById(R.id.img1);
-          nametext=itemView.findViewById(R.id.nametext);
-          descriptiontext=itemView.findViewById(R.id.descriptiontext);
-
-      }
-  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//public class myadapter extends RecyclerView.Adapter<myadapter.MyAdapterViewHolder>{
+//    private Context context;
+//    public ArrayList<model> arrayList;
+//    public myadapter(Context c, ArrayList<model> arrayList){
+//        this.context = c;
+//        this.arrayList = arrayList;
+//    }
+//    @Override
+//    public int getItemCount() {
+//        return 0;
+//    }
+//
+//    @Override
+//    public long getItemId(int position) {
+//        return position;
+//    }
+//
+//    @NonNull
+//    @Override
+//    public MyAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.singlerowdesign,parent,false);
+//        return new myadapter.MyAdapterViewHolder(view);
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull MyAdapterViewHolder holder, int position) {
+//        model model = arrayList.get(position);
+//        final String name = model.getName();
+//        final String description = model.getPropDesc();
+//        final String ownName = model.getmPropOwnerName();
+//        final String ownMail = model.getmPropOwnerMail();
+//        final String price = model.getPrice();
+//        final String countryCode = model.getCCode();
+//        final String address = model.getAddress();
+//        String url = model.getUrl();
+//        holder.nametext.setText(model.getName());
+//        holder.descriptiontext.setText(model.getPropDesc());
+//        Glide.with(holder.img1.getContext()).load(url).into(holder.img1);
+//        holder.itemView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(context, Det.class);
+//                intent.putExtra("Name",name);
+//                intent.putExtra("Desc",description);
+//                intent.putExtra("Url",url);
+//                intent.putExtra("OwnerName", ownName);
+//                intent.putExtra("OwnerMail", ownMail);
+//                intent.putExtra("Price", price);
+//                intent.putExtra("CountryCode", countryCode);
+//                intent.putExtra("Address", address);
+//
+//
+//
+//                context.startActivity(intent);
+//            }
+//        });
+//    }
+//
+//
+//    public class MyAdapterViewHolder extends RecyclerView.ViewHolder{
+//        ImageView img1; TextView nametext; TextView descriptiontext;
+//        public MyAdapterViewHolder(@NonNull View itemView) {
+//            super(itemView);
+////            context = itemView.getContext();
+//          img1=itemView.findViewById(R.id.img1);
+//          nametext=itemView.findViewById(R.id.nametext);
+//          descriptiontext=itemView.findViewById(R.id.descriptiontext);
+//        }
+//    }
+//}
 
 
--- app/src/main/java/com/example/realestateproj/rentals3.java
+++ app/src/main/java/com/example/realestateproj/rentals3.java
@@ -1,57 +1,105 @@
 package com.example.realestateproj;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.SearchView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.core.Context;
+
+import java.util.ArrayList;
 
 public class rentals3 extends AppCompatActivity {
-    private RecyclerView recyclerView;
-    myadapter adapter;
-    DatabaseReference mbase;
 
+    DatabaseReference ref;
+    ArrayList<model> list;
+    RecyclerView recyclerView;
+    SearchView searchView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rentals3);
-        mbase = FirebaseDatabase.getInstance().getReference("Properties").child("Rentals");
+        setContentView(R.layout.activity_sales3);
 
-        recyclerView = findViewById(R.id.recycler2);
+        ref = FirebaseDatabase.getInstance().getReference("Properties").child("Rentals");
+        recyclerView = findViewById(R.id.rv);
+        searchView = findViewById(R.id.searchview);
+        if(ref != null){
+            ref.addValueEventListener(new ValueEventListener() {
+                @NonNull
+                @Override
+                protected Object clone() throws CloneNotSupportedException {
+                    return super.clone();
+                }
 
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if(snapshot.exists()){
+                        list = new ArrayList<>();
+                        for(DataSnapshot ds : snapshot.getChildren()){
+                            list.add(ds.getValue(model.class));
+                        }
+                        myadapter adapterClass = new myadapter(list);
+                        recyclerView.setAdapter(adapterClass);
 
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+                    }
+                }
 
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Toast.makeText(rentals3.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        if (searchView != null){
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    return false;
+                }
 
-        FirebaseRecyclerOptions<model> options
-                = new FirebaseRecyclerOptions.Builder<model>()
-                .setQuery(mbase, model.class)
-                .build();
-
-        adapter = new myadapter(options);
-
-        recyclerView.setAdapter(adapter);
-
-
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    search(newText);
+                    return true;
+                }
+            });
+        }
 
     }
 
-
-    @Override protected void onStart()
-    {
+    @Override
+    protected void onStart() {
         super.onStart();
-        adapter.startListening();
+
+
     }
 
+    private void search(String str) {
+        if (!str.equals("")){
+            ArrayList<model> myList = new ArrayList<>();
+            for (model object : list){
+                if(object.getName().toLowerCase().contains(str.toLowerCase())){
+                    myList.add(object);
+                }
+            }
+            myadapter adapterClass = new myadapter(myList);
+            recyclerView.setAdapter(adapterClass);
+        }
 
-    @Override protected void onStop()
-    {
-        super.onStop();
-        adapter.stopListening();
     }
-
 }
